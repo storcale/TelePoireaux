f7598e36363b4512e4b68c51c25d03408d33fc7e
Ethan Costa <67022946+TathanDev@users.noreply.github.com>
2024-07-24 09:23:17 +0200

    Update turret.js

--- team/turret.js
+++ team/turret.js
@@ -1,11 +1,51 @@
 
-var enemy = getNearestEnemy()
+/**
+ * 
+ * We get the nearest Leek entity
+ * 
+**/
+function customGetNearestEnemy() {
+	
+	var nearestEnemy = [null, null]
+	
+	for(var id = 0; id <= count(getEnemies()); id++) {
+		if (getType(getEnemies()[id]) == ENTITY_LEEK) {
+			
+			var enemy = getEnemies()[id]
+			var distance = getCellDistance(getCell(), getCell(enemy))
 
-while(getLife() < getTotalLife() * 0.75 || getLife(getNearestAlly()) < getTotalLife(getNearestAlly()) * 0.75){
- useChip(CHIP_CURE)
+			//If no enemy is set, set the first one//
+			if (nearestEnemy[0] == null) {
+				nearestEnemy[0] = enemy
+				nearestEnemy[1] = distance
+			} else if ( nearestEnemy[1] >  distance) {
+				nearestEnemy[0] = enemy
+				nearestEnemy[1] = distance
+			}
+		}
+	}
+	return nearestEnemy[0]
 }
-useChip(CHIP_WALL)
-useChip(CHIP_SHIELD)
+
+var enemy = customGetNearestEnemy()
+var ally = getNearestAlly()
+
+while(getLife() < getTotalLife() * 0.75 || getLife(getNearestAlly()) < getTotalLife(getNearestAlly()) * 0.75 ){
+	
+ 	useChip(CHIP_CURE)
+}
+
+/** Attack **/
 useChip(CHIP_VENOM,enemy)
 useChip(CHIP_THORN,enemy)
 
+/** Protect a player or the turret if the turret pourcent of life is lower than 50% **/
+if (100 * getLife() / getTotalLife() < 50) {
+	useChip(CHIP_WALL)
+	useChip(CHIP_SHIELD)
+} else {
+
+	useChip(CHIP_WALL, ally)
+	useChip(CHIP_SHIELD, ally)
+
+}
